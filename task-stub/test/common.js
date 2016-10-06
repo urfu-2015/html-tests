81a6c60f387b74dcbc4a2bb4ecd98af12144f451
Anton Grischenko <f0rmat1k@yandex-team.ru>
2016-10-06 14:27:40 +0500

    Убрал лишние проверки

--- task-stub/test/common.js
+++ task-stub/test/common.js
@@ -9,39 +9,7 @@ var error = require('./error-output');
 var regExps = require('./staff/regExps');
 var utils = require('./staff/utils');
 
-describe('Запрещенные теги.', function () {
-    [ 'i', 'b', 'font', 'center', 'marquee', 'u', 's' ].forEach(function (tag) {
-        it('Не должно быть тега <' + tag + '>', function () {
-            var pattern = regExps.tag(tag);
-            var hasViolation = pattern.test(html);
-
-            if (hasViolation) {
-                error(pattern, 'Использование тега <' + tag + '>.');
-            }
-
-            hasViolation.should.be.eql(false);
-        });
-    });
-});
-
 describe('Сodestyle.', function () {
-    it('Не должно быть табуляций.', function () {
-        var pattern = regExps.tabs();
-        var hasViolation = pattern.test(html);
-
-        if (hasViolation) {
-            error(pattern, 'Символ табуляции.');
-        }
-
-        hasViolation.should.be.eql(false);
-    });
-
-    it('Кол-во пробелов в отступе должно быть кратно четырем.', function () {
-        var found = utils.wrongSpacesChecker(html, true);
-
-        (found).should.be.eql(0);
-    });
-
     it('Не должно быть пробелов после открывающих тегов.', function () {
         var pattern = regExps.spaceAfterTag();
         var hasViolation = pattern.test(html);
@@ -87,32 +55,6 @@ describe('Сodestyle.', function () {
     });
 
     describe('Использование и оформление атрибутов.', function () {
-        describe('Запрещенные атрибуты элементов.', function () {
-            [ 'style', 'border', 'align' ].forEach(function (attr) {
-                it('Не должно быть атрибута ' + attr + '.', function () {
-                    var pattern = regExps.attrs(attr);
-                    var hasViolation = pattern.test(html);
-
-                    if (hasViolation) {
-                        error(pattern, 'Использование атрибута ' + attr + '.');
-                    }
-
-                    hasViolation.should.be.eql(false);
-                });
-            });
-        });
-
-        it('Должны использоваться двойные кавычки значениях атрибутов.', function () {
-            var pattern = regExps.wrongQuoteInAttribute();
-            var hasViolation = pattern.test(html);
-
-            if (hasViolation) {
-                error(pattern, 'Неправильные или отсутствующие кавычки у значений атрибута.');
-            }
-
-            hasViolation.should.be.eql(false);
-        });
-
         it('Не должно быть пробелов после = при использовании атрибутов.', function () {
             var pattern = regExps.spaceAfterEquals();
             var hasViolation = pattern.test(html);
@@ -134,21 +76,6 @@ describe('Сodestyle.', function () {
 
             hasViolation.should.be.eql(false);
         });
-
-        //it('Не должно быть картинок без атрибута alt или с пустым.', function () {
-        //    utils.findImagesWithoutAlt(html).should.be.eql(0);
-        //});
-    });
-
-    it('Не должно быть строк, превышающих 110 символов.', function () {
-        var pattern = regExps.maxLineLength(110);
-        var hasViolation = pattern.test(html);
-
-        if (hasViolation) {
-            error(pattern, 'Строка состоит из более чем 110 символов.');
-        }
-
-        hasViolation.should.be.eql(false);
     });
 
     it('Не должно быть двух и более идущих подряд пустых строк.', function () {
@@ -170,9 +97,5 @@ describe('Сodestyle.', function () {
         it('Не должно быть блочных тегов внутри параграфов (<p>).', function () {
             utils.getBlockInsideP(html).should.be.eql(0);
         });
-    });
-
-    it('Не должно быть закрытых пустых тегов.', function () {
-        utils.getClosedEmptyElements(html).should.be.eql(0);
     });
 });
